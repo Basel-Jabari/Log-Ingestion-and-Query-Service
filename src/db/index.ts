13476254ee547f021db0d2cbb391418280603f54
Basel Al-Jabari <baslimadj@gmail.com>
2026-08-13 00:00:39 +0300

    Create index.ts to Set Up The DB Connection

---
+++ src/db/index.ts
@@ -0,0 +1,7 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import { config } from "../config.js";
+import postgres from "postgres";
+import * as schema from "./schema.js";
+
+const client = postgres(config.db.url);
+export const db = drizzle(client, { schema });
