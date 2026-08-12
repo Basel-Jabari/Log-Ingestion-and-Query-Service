4a0d67b75272226224ba5f1c5f7d7e3acabbed53
basel-3mk <baslimadj@gmail.com>
2026-08-13 01:32:41 +0300

    Initializing The App

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { config } from "./config.js";
+
+export const app = express();
+
+// The default limit is 100kb
+// Since large batch of logs could exceed that
+// I configured it to be higher in default
+app.use(
+    express.json({
+        limit: config.server.bodyLimit
+    })
+);
