e5cdb31e3a486a2cf21e24786e3c4c3c27f5ae89
basel-3mk <baslimadj@gmail.com>
2026-08-13 21:45:35 +0300

    Add Auto-Migration Feature + Starting the Server in a Better Way

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import { config } from "./config.js";
 import { app } from "./app.js";
+import postgres from "postgres";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { once } from "node:events";
 
-app.listen(config.server.port, () => {
-    console.log(`Server is running at http://localhost:${config.server.port}`);
-});
+const main = async () => {
+    const migrationClient = postgres(config.db.url, {
+        max: 1
+    });
+
+    try {
+        await migrate(drizzle(migrationClient), config.db.migrationConfig);
+        console.log("Database migrations completed successfully.");
+    } finally {
+        await migrationClient.end();
+    }
+
+    const server = app.listen(config.server.port);
+    await once(server, "listening");
+    config.server.isReady = true;
+    console.log(`Server is running at http://localhost:${config.server.port}.`);
+};
+
+main();
