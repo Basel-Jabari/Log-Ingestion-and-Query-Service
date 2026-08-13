e22e9c0cc28d5c92debebc549e59fe2b9c5b7b74
basel-3mk <baslimadj@gmail.com>
2026-08-13 20:44:19 +0300

    Let the Application Uses the Error Handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { config } from "./config.js";
 import { app } from "./app.js";
+import { middlewareErrorHandler } from "./middlewares/error_handler.js";
 
 app.listen(config.server.port, () => {
     console.log(`Server is running at http://localhost:${config.server.port}`);
 });
+
+app.use(middlewareErrorHandler);
