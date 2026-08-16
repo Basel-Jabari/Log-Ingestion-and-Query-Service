c0024b65ca3b21f045477a0eb1da1b5abbd68f86
basel-3mk <baslimadj@gmail.com>
2026-08-16 20:48:45 +0300

    Add max pool and prepare to db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,5 +3,10 @@ import { config } from "../config.js";
 import postgres from "postgres";
 import * as schema from "./schema.js";
 
-const client = postgres(config.db.url);
+const client = postgres(config.db.url, {
+    max: config.db.poolMax,
+    prepare: true
+});
+
 export const db = drizzle(client, { schema });
+export const pgClient = client;
